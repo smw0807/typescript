bacfbb56e9eb71b906a187eb04478289ab0061d1
Minu Song <smw0807@gmail.com>
2023-07-25 23:52:00 +0900

    책 > 예제 소스

---
+++ typescript-book/10/bc_app/src/server/simple-websocket-server.ts
@@ -0,0 +1,27 @@
+import * as express from 'express';
+import * as path from 'path';
+import { Server } from 'ws';
+
+const app = express();
+
+// HTTP Server
+app.get('/', (req, res) => {
+  return res.sendFile(
+    path.join(__dirname, '../../public/simple-websocket-client.html')
+  );
+});
+
+const httpServer = app.listen(8000, 'localhost', () => {
+  console.log('HTTP server is listening on localhost:8000');
+});
+
+// Websocket Server
+const wsServer = new Server({ port: 8085 });
+console.log('WebSocket server is listening on localhost:8085');
+
+wsServer.on('connection', (wsClient) => {
+  wsClient.send('This message was pushed by the WebSocket server');
+
+  wsClient.onerror = (error) =>
+    console.error(`The server receive: ${error['code']}`);
+});
