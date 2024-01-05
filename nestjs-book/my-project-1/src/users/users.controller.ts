1418e5bb9ae23d4d5ef67fe706c5eb1a08bf4465
송민우 <smw0807@gmail.com>
2024-01-05 15:23:23 +0900

    책따라하기

--- nestjs-book/my-project-1/src/users/users.controller.ts
+++ nestjs-book/my-project-1/src/users/users.controller.ts
@@ -1,7 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Res,
+  BadRequestException,
+  Header,
+  Redirect,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Response } from 'express';
 
 @Controller('users')
 export class UsersController {
@@ -13,12 +26,21 @@ export class UsersController {
   }
 
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  findAll(@Res() res: Response) {
+    /**
+     * @Res 데코레이터를 이용해 응답객체를 다룰 수도 있다.
+     */
+    const users = this.usersService.findAll();
+    return res.status(200).send(users);
   }
 
+  @Redirect('http://localhost:3000/users', 301)
+  @Header('Custom', 'Test Header')
   @Get(':id')
   findOne(@Param('id') id: string) {
+    if (+id < 1) {
+      throw new BadRequestException('id는 0보드 큰 값이어야 합니다.');
+    }
     return this.usersService.findOne(+id);
   }
 
@@ -30,5 +52,17 @@ export class UsersController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
+  }
+
+  @Delete(':id/memo/:memoId')
+  deleteUserMemo(
+    @Res() res: Response,
+    @Param('id') id: string,
+    @Param('memoId') mId: string,
+  ) {
+    return res.status(200).send({
+      id: id,
+      memoId: mId,
+    });
   }
 }
