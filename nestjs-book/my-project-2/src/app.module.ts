40fb15d1ec2a9ee580f08eef9378523d69792455
smw0807 <smw0807@gmail.com>
2024-02-24 18:29:15 +0900

    프로바이더추가

--- nestjs-book/my-project-2/src/app.module.ts
+++ nestjs-book/my-project-2/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
+import { EmailService } from './email/email.service';
+import { UsersService } from './users/users.service';
 
 @Module({
   imports: [],
   controllers: [AppController, UsersController],
-  providers: [AppService],
+  providers: [AppService, UsersService, EmailService],
 })
 export class AppModule {}
