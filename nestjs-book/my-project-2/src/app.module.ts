4db90aef1e2b1e87c4275a9ab9863d35ff91f950
smw0807 <smw0807@gmail.com>
2024-02-26 00:48:00 +0900

    ConfigModule을 통한 환경변수 적용

--- nestjs-book/my-project-2/src/app.module.ts
+++ nestjs-book/my-project-2/src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import emailConfig from './config/emailConfig';
+import { validationSchema } from './config/vaildationSchema';
 
 @Module({
-  imports: [CoreModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: process.env.NODE_ENV === 'development' ? '.env.development' : '.env.production',
+      load: [emailConfig],
+      isGlobal: true,
+      validationSchema,
+    }),
+    CoreModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
