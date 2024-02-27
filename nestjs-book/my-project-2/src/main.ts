7142c09c61b5e4b3c782eb4030f8dbff5307ebeb
smw0807 <smw0807@gmail.com>
2024-02-28 00:00:16 +0900

    글로벌파이프 설정

--- nestjs-book/my-project-2/src/main.ts
+++ nestjs-book/my-project-2/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-// import { ValidationPipe } from './common/validation.pipe';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,7 +9,12 @@ async function bootstrap() {
   const port = configService.get('APP_PORT');
 
   // 글로벌 파이프 설정
-  // app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      //class-transformer를 적용되게 하려면 transform 속성을 true로 주어야 한다.
+      transform: true,
+    }),
+  );
   await app.listen(port || 3000);
 }
 bootstrap();
