7402a2b06bd7f37e9f52f4d3dc3b7e951e680e7c
smw0807 <smw0807@gmail.com>
2024-02-28 00:02:24 +0900

    dto 유효성 검사

--- nestjs-book/my-project-2/src/users/dto/create-user.dto.ts
+++ nestjs-book/my-project-2/src/users/dto/create-user.dto.ts
@@ -1,12 +1,17 @@
-import { IsString, MinLength, MaxLength, IsEmail } from 'class-validator';
+import { IsString, MinLength, MaxLength, IsEmail, Matches } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
-  @MinLength(1)
-  @MaxLength(20)
+  @MinLength(2)
+  @MaxLength(30)
   readonly name: string;
 
+  @IsString()
   @IsEmail()
+  @MaxLength(60)
   readonly email: string;
+
+  @IsString()
+  @Matches(/^[A-Za-z\d!@#$%^&*()]{8, 30}$/) //영문 대소문자 또는 특수문자로 이뤄진 8~30자 문자열
   readonly password: string;
 }
