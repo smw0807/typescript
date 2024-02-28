ff5b9b40370d6505f4871b6d6f71a53d456da2af
smw0807 <smw0807@gmail.com>
2024-02-28 23:57:21 +0900

    데코레이터를 이용한 커스텀 유효성 검사

---
+++ nestjs-book/my-project-2/src/utils/not-in.ts
@@ -0,0 +1,24 @@
+import { ValidationOptions, registerDecorator, ValidationArguments } from 'class-validator';
+
+export function NotIn(property: string, validationOptions?: ValidationOptions) {
+  return (object: object, propertyName: string) => {
+    registerDecorator({
+      name: 'NotIn',
+      target: object.constructor,
+      propertyName,
+      options: validationOptions,
+      constraints: [property],
+      validator: {
+        validate(value: any, args: ValidationArguments) {
+          const [relatedPropertyName] = args.constraints;
+          const relatedValue = (args.object as any)[relatedPropertyName];
+          return (
+            typeof value === 'string' &&
+            typeof relatedValue === 'string' &&
+            !relatedValue.includes(value)
+          );
+        },
+      },
+    });
+  };
+}
