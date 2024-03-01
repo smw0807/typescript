29c010dff7ef7114c281edd943628c8bf9d92b4a
smw0807 <smw0807@gmail.com>
2024-03-01 21:00:37 +0900

    TypeORM 적용

--- nestjs-book/my-project-2/src/app.module.ts
+++ nestjs-book/my-project-2/src/app.module.ts
@@ -6,15 +6,18 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import emailConfig from './config/emailConfig';
 import { validationSchema } from './config/vaildationSchema';
+import dbConfig from './config/dbConfig';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: process.env.NODE_ENV === 'development' ? '.env.development' : '.env.production',
-      load: [emailConfig],
+      load: [emailConfig, dbConfig],
       isGlobal: true,
       validationSchema,
     }),
+    DatabaseModule,
     CoreModule,
     UsersModule,
   ],
