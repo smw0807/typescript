c6ad14a533b043e2e1a4f52d39b1762b7b3db651
smw0807 <smw0807@gmail.com>
2024-03-03 16:49:37 +0900

    가드 적용

--- nestjs-book/my-project-2/src/users/users.controller.ts
+++ nestjs-book/my-project-2/src/users/users.controller.ts
@@ -1,15 +1,17 @@
-import { Body, Controller, Post, Logger, Query } from '@nestjs/common';
+import { Body, Controller, Post, Logger, Query, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { VerifyEmailDto } from './dto/verify-email.dto';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UsersService } from './users.service';
 import { ValidationPipe } from 'src/common/validation.pipe';
+import { AuthGuard } from 'src/guard/authGuard';
 
 @Controller('users')
 export class UsersController {
   private readonly logger = new Logger(UsersController.name);
   constructor(private usersService: UsersService) {}
 
+  @UseGuards(AuthGuard)
   @Post() //{{api}}/users
   async createUser(@Body(ValidationPipe) dto: CreateUserDto): Promise<void> {
     const { name, email, password } = dto;
