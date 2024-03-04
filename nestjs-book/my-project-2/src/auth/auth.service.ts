dc6154315769fec66c9cd9fff5d6fa7d0c2c0534
smw0807 <smw0807@gmail.com>
2024-03-04 22:03:33 +0900

    AuthModule 추가 토큰 발급 및 토큰 검증

---
+++ nestjs-book/my-project-2/src/auth/auth.service.ts
@@ -0,0 +1,43 @@
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import authConfig from 'src/config/authConfig';
+import * as jwt from 'jsonwebtoken';
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+@Injectable()
+export class AuthService {
+  constructor(@Inject(authConfig.KEY) private readonly config: ConfigType<typeof authConfig>) {}
+
+  /**
+   * 토큰 발급
+   * @param user 사용자 정보
+   * @returns 토큰
+   */
+  login(user: User) {
+    const payload = { ...user };
+    return jwt.sign(payload, this.config.jwtSecret, {
+      expiresIn: '1d',
+      audience: 'example.com',
+      issuer: 'example.com',
+    });
+  }
+
+  // 토큰 검증
+  verify(jwtString: string) {
+    try {
+      const payload = jwt.verify(jwtString, this.config.jwtSecret) as (jwt.JwtPayload | string) &
+        User;
+      const { id, email } = payload;
+      return {
+        userId: id,
+        email,
+      };
+    } catch (e) {
+      throw new UnauthorizedException();
+    }
+  }
+}
