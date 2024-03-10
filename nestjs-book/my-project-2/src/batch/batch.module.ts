f4e1a49fc79195c6934157c2b02cd4f117eb3c65
smw0807 <smw0807@gmail.com>
2024-03-10 21:32:39 +0900

    동적 태스크 스케줄링

--- nestjs-book/my-project-2/src/batch/batch.module.ts
+++ nestjs-book/my-project-2/src/batch/batch.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
-import { TaskService } from './batch.service';
+import { BatchContorller } from './batch.controller';
+import { TaskService2 } from './task.service';
+// import { TaskService } from './batch.service';
 
 @Module({
   imports: [ScheduleModule.forRoot()],
-  providers: [TaskService],
+  providers: [
+    // TaskService
+    TaskService2,
+  ],
+  controllers: [BatchContorller],
 })
 export class BatchModule {}
