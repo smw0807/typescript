cff820c3ee6c50143eb01348fabe3e66f2f4516a
smw0807 <smw0807@gmail.com>
2024-03-11 23:44:41 +0900

    DB 헬스 체크

--- nestjs-book/my-project-2/src/healthCheck/health.check.controller.ts
+++ nestjs-book/my-project-2/src/healthCheck/health.check.controller.ts
@@ -1,16 +1,28 @@
 import { Controller, Get } from '@nestjs/common';
-import { HealthCheck, HealthCheckService, HttpHealthIndicator } from '@nestjs/terminus';
+import {
+  HealthCheck,
+  HealthCheckService,
+  HttpHealthIndicator,
+  TypeOrmHealthIndicator,
+} from '@nestjs/terminus';
 
 @Controller('health-check')
 export class HealthCheckController {
   constructor(
     private health: HealthCheckService,
     private http: HttpHealthIndicator,
+    private db: TypeOrmHealthIndicator,
   ) {}
 
   @Get('/ping-check')
   @HealthCheck()
   check() {
     return this.health.check([() => this.http.pingCheck('nestjs-docs', 'https://docs.nestjs.com')]);
+  }
+
+  @Get('/db-check')
+  @HealthCheck()
+  checkDb() {
+    return this.health.check([() => this.db.pingCheck('database')]);
   }
 }
